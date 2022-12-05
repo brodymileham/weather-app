bdf0ffea79c21d148c2aa2348e7a83253116fb95
Brody Mileham <37845540+brodymileham@users.noreply.github.com>
2022-12-05 18:23:23 +0000

    Added HTML CSS and JS Files

---
+++ main.js
@@ -0,0 +1,31 @@
+let weather = {
+    "apiKey" : "b533a6ebfb0410e859eddd7f4aec61e4",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
+            ).then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity }  = data.main;
+        const { speed } = data.wind;
+
+
+        console.log(name, icon, description, temp, speed);
+        document.querySelector(".city").innerText = "The Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity;
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " M/s";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function(){
+        weather.search();
+});
